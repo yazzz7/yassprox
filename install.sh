49a30b12d2e9e7973a71e5536abb22b2c6bfdeea
yazzz7 <152229669+yazzz7@users.noreply.github.com>
2023-12-03 19:31:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ENDCOLOR="\e[0m"
 clear
 echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
 sleep 1
-if [ -f "proxy" ]; then
+if [ -f "ilyas" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
     rm proxy
     sleep 1
@@ -14,5 +14,5 @@ fi
 wget -q https://github.com/yazzz7/yassprox/raw/main/proxy
 sleep 1
 echo -e "${GREEN}Ilyas Proxy is now Installed.${ENDCOLOR}"
-echo -e "${GREEN}Execute proxy with this command: ./proxy${ENDCOLOR}"
-chmod +x proxy
+echo -e "${GREEN}Execute proxy with this command: ./ilyas${ENDCOLOR}"
+chmod +x ilyas
